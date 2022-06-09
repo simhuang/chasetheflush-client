88d3abba6d050eb5db1a41d3c831f56a91bc792a
Simon Huang <simhuang92@gmail.com>
2022-06-09 00:22:09 -0400

    fetch comments

--- src/api/paths.ts
+++ src/api/paths.ts
@@ -1,6 +1,7 @@
 export const BASE_PATH = "http://localhost:8000/v1";
 export const DISCUSSIONS = "/discussions";
 export const POST = "/posts";
+export const COMMENT = "/comments";
 
 export const getpaginatedDisucssionsPath = (limit = 0, offset = 0) => {
   return `${BASE_PATH}${DISCUSSIONS}?limit=${limit}&offset=${offset}`;
@@ -8,4 +9,8 @@ export const getpaginatedDisucssionsPath = (limit = 0, offset = 0) => {
 
 export const getPost = (id: number) => {
   return `${BASE_PATH}${DISCUSSIONS}/${id}`;
+}
+
+export const getComments = (id: number) => {
+  return `${BASE_PATH}${COMMENT}`;
 }
--- src/containers/PostContainer.tsx
+++ src/containers/PostContainer.tsx
@@ -1,14 +1,42 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+
+import { getComments } from 'src/api/paths';
 
 const PostContainer: FunctionComponent<PostContainerTypes> = ({post}) => {
+    const [comments, setComments] = useState([])
+
+    useEffect(() => {
+        fetchComments()
+    }, [])
+
+    const fetchComments = async () => {
+        const response = await fetch(getComments(0))
+        const comments = await response.json()
+        setComments(comments.data)
+    }
+
+    const renderComments = () => {
+        return comments.forEach((comment) => {
+            return (
+                <div>
+                    <div>{comment.comment}</div>
+                    <div>{comment.upvotes}</div>
+                </div>
+            )
+        })
+    }
+
     return (
         <>
-            <div>hellowodfdrld</div>
+            <h3>post</h3>
             <div>{post.title}</div>
             <div>{post.content}</div>
             <div>{post.upvotes}</div>
             <div>{post.commentCount}</div>
             <div>{post.created}</div>
+            <br/>
+            <h3>comments</h3>
+            {renderComments()}
         </>
     )
 }
